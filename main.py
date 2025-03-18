4c153144ef48e6c458d7a32be82fb3820f990108
Diogo Beijinha <76941417+diogo-beijinha@users.noreply.github.com>
2025-03-18 21:32:57 +0000

    Added MD5 hashing

--- main.py
+++ main.py
@@ -2,7 +2,29 @@ import argparse
 import os
 import shutil
 import logging
+import hashlib
 
+class SyncFolders:
+
+    def parse_args(self):
+        pass
+
+    def create_replicas(self, source, replica):
+        pass
+
+def get_file_md5(filepath):
+    md5 = hashlib.md5()
+
+    # handle content in binary form
+    f = open(filepath, "rb")
+
+    while chunk := f.read(4096):
+        md5.update(chunk)
+    
+    return md5.hexdigest()
+
+
+# parse args
 parser = argparse.ArgumentParser(description="Sync two folders")
 
 # Arguments list
@@ -20,38 +42,32 @@ logging.basicConfig(filename=args.log,
 
 # print(f"Interval: {args.interval}")
 
-
+# create replicas
 sourceFiles = os.listdir(args.source)
 replicaFiles = os.listdir(args.replica)
 # print(replicaFiles)
 
 logs = open(args.log, "a")
 
-# iterate through files
+# iterate through files for removal
+for filename in os.listdir(args.replica):
+    file_path = os.path.join(args.replica, filename)
+    if os.path.isfile(file_path):
+        # DELETE FILE FROM REPLICAS IF NOT IN SOURCE
+        if filename not in sourceFiles or filename in sourceFiles and get_file_md5(file_path) != get_file_md5(os.path.join(args.source, filename)):
+            print(f"{filename} not in source")
+            logging.warning(f"File {filename} does not exist in Source folder! Deleting from replicas..\n")
+            os.remove(file_path)
+
+# iterate through files for creation
 for filename in os.listdir(args.source):
     file_path = os.path.join(args.source, filename)
     if os.path.isfile(file_path):
         # FILE IS IN REPLICAS
-        if filename in replicaFiles:
+        if filename in replicaFiles and get_file_md5(file_path) == get_file_md5(os.path.join(args.replica, filename)):
             logging.info(f"Source file {filename} found in replicas folder!\n")
         # FILE IS NOT IN REPLICAS
         else:
             shutil.copy(file_path, os.path.join(args.replica, filename))
             logging.info(f"Source file {filename} created in replicas folder!\n")
 
-for filename in os.listdir(args.replica):
-    file_path = os.path.join(args.replica, filename)
-    if os.path.isfile(file_path):
-        # FILE IS IN REPLICAS
-        if filename not in sourceFiles:
-            os.remove(file_path)
-            print(f"{filename} not in source")
-            logging.warning(f"File {filename} does not exist in Source folder! Deleting from replicas..\n")
-
-
-# name != name SKIP >> DONE <<
-# source not in replica == create file in replica OVER >> DONE <<
-# replica not in source == delete replica file OVER >> DONE <<
-
-# name == name => check hash => hash is the same OVER
-# name == name => check hash => hash is not the same ==> delete replica file ==> create source file in replica OVER
